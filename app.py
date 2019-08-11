0842080f0648915b127235c6d349fadc040e02e5
bschoney <bschoney@utexas.edu>
2019-08-11 09:05:32 -0500

    config setup in app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restless import APIManager
+from flask_cors import CORS
+import os
+
+# --- Database Connection Info --- #
+class Config (object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://dbd:dbdpassword@dbd.chncgtkyhkgz.us-east-2.rds.amazonaws.com:5432/dbd"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+CORS(app)
